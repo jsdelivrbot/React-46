9201dbaa786d5ed3d9f458f65fe919e488101a77
doo9392 <hackish@hanmail.net>
2017-12-08 10:56:33 +0900

    React-udemy
    React-udemy

--- udemy/ReduxSimpleStarter/src/index.js
+++ udemy/ReduxSimpleStarter/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-	return <div>Hello world!</div>;
+	return <div>Hello world!!!</div>;
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));

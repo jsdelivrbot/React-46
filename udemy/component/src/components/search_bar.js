d400c961c015bb9b0cb1dbff82c955aa46fc9b85
doo9392 <hackish@hanmail.net>
2017-12-11 15:22:04 +0900

    React-udemy
    React-udemy

--- udemy/component/src/components/search_bar.js
+++ udemy/component/src/components/search_bar.js
@@ -2,10 +2,11 @@ import React, {Component} from 'react';
 
 class SearchBar extends Component {
   constructor(props) {
-     super(props);
+    super(props);
 
     this.state = {term: ''};
   }
+  
   render() {
     return (
       <div >
--- udemy/component/src/index.js
+++ udemy/component/src/index.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_Key = 'AIzaSyCAyX88dbV4aoKaftekdl4_j8L1A_gx6I8';
 
-YTSearch({key : API_Key, trem : 'surfboards'}, function(data){
-	console.log(data);
-});
+class App extends Component{
+	constructor(props) {
+		super(props);
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+		this.state = {videos : []};
+		
+		YTSearch({key : API_Key, trem : 'surfboards'}, (videos) => {
+			this.setState({videos}); //this.setState({videos : data});
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetail video={this.state.videos[0]} />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));

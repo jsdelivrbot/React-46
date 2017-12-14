41f1fa3504d071fbe8bb4c87ebeb7c733cb4f62a
doo9392 <hackish@hanmail.net>
2017-12-14 18:34:30 +0900

    React-udemy
    React-udemy

---
+++ udemy/blog-router/src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+    render() {
+        return (
+            <div>
+                post new
+            </div>
+        );
+    }
+}
+
+export default PostsNew;
